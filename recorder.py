71bb9b60543121841170b807e6eb5ddeff05a0c1
Brinnyman <>
2018-12-15 21:31:40 +0100

    Refactored code, using popen to pipe streamlink output to ffmpeg

--- recorder.py
+++ recorder.py
@@ -7,9 +7,9 @@ import subprocess
 import time
 
 
-class Recorder:
+class StreamRecorder:
     def __init__(self):
-        self.twitch_client_id = "jzkbprff40iqj646a697cyrvl0zt2m6"  # don't change this
+        self.twitch_client_id = "jzkbprff40iqj646a697cyrvl0zt2m6"  # don't change this TODO: Maybe change this because everybody uses this ID
         self.refresh = 15.0
         self.name = ""  # recording directory and twitch username
         self.type = ""  # recording type <twitch, vod, repair>
@@ -17,79 +17,64 @@ class Recorder:
         self.vodid = ""  # twitch vod id
         self.quality = "best"  # recording quality, default is best <best, high, low, medium, mobile, source, worst>
         self.recordpath = ""  # record path
-        self.processpath = ""  # process path
-        self.command = ""  # streamlink commands
+        self.streamlink_commands = ""  # streamlink commands
         self.ffmpeg_path = 'ffmpeg'  # path to ffmpeg executable
         self.streamlink_path = 'streamlink'  # path to streamlink executable
 
-    def setup(self):
+    def create_directory(self):
         self.recorded_path = os.path.join(os.path.abspath(self.recordpath), "recorded", self.name)
         if(os.path.isdir(self.recorded_path) is False):
             os.makedirs(self.recorded_path)
-        self.processed_path = os.path.join(os.path.abspath(self.processpath), "processed", self.name)
-        if(os.path.isdir(self.processed_path) is False):
-            os.makedirs(self.processed_path)
 
         if(self.refresh < 15):
             print("Check interval should not be lower than 15 seconds.")
             self.refresh = 15
             print("System set check interval to 15 seconds.")
 
-    def create_files(self, name, date):
+    def create_filename(self, name, date):
         filename = name + "_" + date + ".mp4"
         filename = "".join(x for x in filename if x.isalnum() or x in ["-", "_", "."])
-        recorded_filename = os.path.join(self.recorded_path, filename)
-        processed_filename = os.path.join(self.processed_path, filename)
-        return recorded_filename, processed_filename
-
-    def clean_files(self, recorded_filename, processed_filename):
-        print('Check if recorded file needs to be repaired.')
-        if(os.path.exists(recorded_filename) is True):
-            try:
-                print("Repair video file.")
-                subprocess.call([self.ffmpeg_path, '-err_detect', 'ignore_err', '-i', recorded_filename, '-c', 'copy', processed_filename])
-                print("Repaired video file.")
-                os.remove(recorded_filename)
-            except Exception as e:
-                print(e)
-        else:
-            print("Skip repairing. File not found.")
+        filename = os.path.join(self.recorded_path, filename)
+        return filename
 
     def record(self, url, filename, *args):
-        print("Recording in session.")
-        print(self.streamlink_path, url, self.quality, "-o", filename, list(args))
-        subprocess.call([self.streamlink_path, url, self.quality, "-o", filename] + list(args))
-        print("Recording is done.")
-
-    def run(self):
-        self.setup()
-        print('Start recorder')
+        process = None
 
         try:
-            print('Check if there are files in the recorded folder.')
-            video_list = [f for f in os.listdir(self.recorded_path) if os.path.isfile(os.path.join(self.recorded_path, f))]
-            if(len(video_list) > 0):
-                print('Repairing previously recorded files.')
-            for f in video_list:
-                recorded_filename = os.path.join(self.recorded_path, f)
-                processed_filename = os.path.join(self.processed_path, f)
-                if(os.path.exists(recorded_filename) is True):
-                    self.clean_files(recorded_filename, processed_filename)
-                else:
-                    print("Skip repairing. File not found.")
-        except Exception as e:
-            print(e)
+            print('Recording in session.')
+            
+            print('start streamlink')
+            streamlink = [self.streamlink_path, url, self.quality, '--stdout'] + list(args)
+            process = subprocess.Popen(streamlink, stdout=subprocess.PIPE, stderr=None)
 
-        if self.type == 'repair':
-            return
-        elif self.type == 'twitch':
+            # TODO: -bsf h264_mp4toannexb? https://www.ffmpeg.org/ffmpeg-bitstream-filters.html#h264_005fmp4toannexb
+            print('start ffmpeg')
+            ffmpeg = [self.ffmpeg_path, '-i', 'pipe:0', '-vcodec', 'libx264', '-acodec', 'aac', '-f', 'mpegts', filename, '-loglevel', 'warning']
+            process2 = subprocess.Popen(ffmpeg, stdin=process.stdout, stdout=subprocess.PIPE, stderr=None)
+
+            process.stdout.close()
+            process2.communicate()
+            print('Recording is done.')
+
+        except OSError:
+            print('An error has occurred while trying to use livestreamer package. Is it installed? Do you have Python in your PATH variable?')
+            sys.exit(1)
+
+        # TODO: remove stdout?
+        return process2.stdout
+
+    def run(self):
+        print('Start StreamRecorder')
+        self.create_directory()
+
+        if self.type == 'twitch':
             self.record_twitch_stream()
         elif self.type == 'vod':
             self.record_twitch_vod()
         else:
             self.record_stream()
 
-    def check_twitch_stream(self):
+    def check_twitch_stream_status(self):
         api = 'https://api.twitch.tv/kraken/streams/' + self.name
         info = None
         status = 3  # 3: error
@@ -98,14 +83,15 @@ class Recorder:
             r.raise_for_status()
             info = r.json()
             if info['stream'] is None:
-                status = 1  # 1: offline
-            elif info['stream']['stream_type'] == 'watch_party':
-                status = 4  # 4: vodcast
-            else:
-                status = 0  # 0: online
+                print("stream is offline")
+                status = 0
+            elif info['stream']['stream_type'] == 'live':
+                print("stream is online")			
+                status = 1
         except requests.exceptions.RequestException as e:
             if e.response:
                 if e.response.reason == 'Not Found' or e.response.reason == 'Unprocessable Entity':
+                    print("stream not found")
                     status = 2  # 2: not found
         return status
 
@@ -118,80 +104,60 @@ class Recorder:
         return info
 
     def record_twitch_stream(self):
-        self.command = "--twitch-disable-hosting"
+        self.streamlink_commands = "--twitch-disable-hosting"
         while True:
-            status = self.check_twitch_stream()
-            if status == 2:
-                print("Stream not found. Invalid stream or typo.")
-                time.sleep(self.refresh)
-            elif status == 3:
-                print(datetime.datetime.now().strftime("%Hh%Mm%Ss"), " ", "unexpected error. will try again in 5 minutes.")
-                time.sleep(300)
-            elif status == 4:
-                print(self.name, "currently vodcasting, checking again in", self.refresh, "seconds.")
-                time.sleep(self.refresh)
-            elif status == 1:
-                print(self.name, "currently offline, checking again in", self.refresh, "seconds.")
-                time.sleep(self.refresh)
-            elif status == 0:
-                print(self.name, "online. Stream recording in session.")
-
-                recorded_filename, processed_filename = self.create_files(self.name, datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss"))
+            print('check twitch stream status')
+            status = self.check_twitch_stream_status()
+            if status == 1:
+                recorded_filename = self.create_filename(self.name, datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss"))
                 self.url = 'twitch.tv/' + self.name
-                self.record(self.url, recorded_filename, self.command)
-                self.clean_files(recorded_filename, processed_filename)
-
+                self.record(self.url, recorded_filename, self.streamlink_commands)
                 time.sleep(self.refresh)
 
     def record_twitch_vod(self):
         info = self.check_twitch_vod()
-        recorded_filename, processed_filename = self.create_files(info['channel']['name'], info['published_at'])
+        filename = self.create_filename(info['channel']['name'], info['published_at'])
         self.url = 'twitch.tv/videos/' + self.vodid
-        self.record(self.url, recorded_filename)
-        self.clean_files(recorded_filename, processed_filename)
+        self.record(self.url, filename)
 
     def record_stream(self):
         while True:
-            recorded_filename, processed_filename = self.create_files(self.name, datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss"))
-            self.record(self.url, recorded_filename)
-            self.clean_files(recorded_filename, processed_filename)
+            filename = self.create_filename(self.name, datetime.datetime.now().strftime("%Y-%m-%d_%Hh%Mm%Ss"))
+            self.record(self.url, filename)
             time.sleep(self.refresh)
 
 
 def main(argv):
     """Exectute command line options."""
-    recorder = Recorder()
+    recorder = StreamRecorder()
 
     usage = 'Usage: recorder.py [options]\n'
     usage += '\n'
     usage += 'Options:\n'
     usage += '-h, --help        prints this message\n'
     usage += '-n, --name        recording directory and twitch username\n'
-    usage += '-t, --type        recording type <twitch, vod, repair>\n'
+    usage += '-t, --type        recording type <twitch, vod>\n'
     usage += '-u, --url         url\n'
     usage += '-v, --vod         twitch vod id\n'
     usage += '-q, --quality     recording quality, default is best\n'
     usage += '-r, --recordpath  record path\n'
-    usage += '-p, --processpath process path\n'
     usage += '-c, --command     streamlink command\n'
     usage += '\n'
     usage += 'Examples:\n'
-    usage += 'Recording stream: recorder.py -n name -u url -q 720p -r /recording -p /processing\n'
-    usage += 'Repairing files in the recorded directory:\nrecorder.py -n name -t repair -r /recording -p /processing'
+    usage += 'Recording stream: recorder.py -n name -u url -q 720p -r /recording\n'
     usage += 'Recording twitch stream:\nrecorder.py -n username -t twitch -q best -c --twitch-disable-hosting\n'
     usage += 'Recording twitch vod:\nrecorder.py -n username -t vod -v 13245678\n'
 
     try:
-        options, remainder = getopt.getopt(sys.argv[1:], 'hn:u:t:v:q:r:p:c:', ['name=',
+        options = getopt.getopt(sys.argv[1:], 'hn:u:t:v:q:r:p:c:', ['name=',
                                                                                'url=',
                                                                                'type=',
                                                                                'vod=',
                                                                                'quality=',
                                                                                'recordpath=',
-                                                                               'processpath=',
                                                                                'command='])
     except getopt.GetoptError as e:
-        print(usage)
+        print(e, usage)
         sys.exit(2)
 
     for opt, arg in options:
@@ -210,13 +176,10 @@ def main(argv):
             recorder.quality = arg
         elif opt in ('-r', '--recordpath'):
             recorder.recordpath = arg
-        elif opt in ('-p', '--processpath'):
-            recorder.processpath = arg
         elif opt in ('-c', '--command'):
             recorder.command = arg
 
     recorder.run()
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
